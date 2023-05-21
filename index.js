492d170c17915609b475c53061d624206e538521
Josh <joshh2201@gmail.com>
2023-05-21 00:06:44 -0400

    Add method to gameBoard to check for a win
    checkWin is called everytime updateGameBoard is called. checkWin returns
    true if the row, column, or diagonal of the updated position all contain
    the provided symbol and returns false otherwise.

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 const gameBoard = (() => {
-  const board = [...Array(9)];
+  const board = new Array(9);
+  const checkWin = (position, symbol) => {
+    let diagWin = false;
+    let colWin = true;
+    let rowWin = true;
+    // diagonal win is impossible without the center square
+    const centerSquare = board[4];
+    if (centerSquare === symbol && position % 2 === 0) {
+      diagWin =
+        (board[0] === board[8] && board[0] === symbol) ||
+        (board[2] === board[6] && board[2] === symbol);
+    }
+    // colStart is the index of the element in the first row of a col
+    let colStart = position % 3;
+    while (colStart < 9) {
+      if (board[colStart] !== symbol) colWin = false;
+      colStart += 3;
+    }
+    // rowStart is the index of the element in the first col of a row
+    const rowStart = position - (position % 3);
+    console.log(rowStart);
+    let j = 0;
+    while (j < 3) {
+      if (board[rowStart + j] !== symbol) rowWin = false;
+      j += 1;
+    }
+    return colWin || rowWin || diagWin;
+  };
   const updateGameBoard = (position, symbol) => {
     board[position] = symbol;
-    console.log(board);
+    if (checkWin(position, symbol)) {
+      console.log('win');
+    } else {
+      console.log('play on');
+    }
   };
   return { updateGameBoard };
 })();
