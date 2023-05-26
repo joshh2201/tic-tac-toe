c18c161e30082a07dc50f2bafa648e2bb120ef7b
Josh <joshh2201@gmail.com>
2023-05-25 20:20:31 -0400

    Modify logic to toggle form for consecutive clicks

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 function showForm() {
   const formDiv = this.nextElementSibling;
+  const formShowing = formDiv.style.display === 'block';
   [...document.querySelectorAll('form')].forEach((form) => {
     form.style.display = '';
   });
-  formDiv.style.display = 'block';
+  if (!formShowing) {
+    formDiv.style.display = 'block';
+  }
 }
 
 [...document.querySelectorAll('.toggle')].forEach((btn) => {
