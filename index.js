59894519925d743780ef3160a8b9807d63f022eb
Josh <joshh2201@gmail.com>
2023-05-27 21:55:23 -0400

    Disable certain eslint rules

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-undef */
 function showForm() {
   const formDiv = this.nextElementSibling;
   const formShowing = formDiv.style.display === 'block';
   [...document.querySelectorAll('form')].forEach((form) => {
+    // eslint-disable-next-line no-param-reassign
     form.style.display = '';
   });
   if (!formShowing) {
